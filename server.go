6308afef6bf803ad75c3e8da9b32d6848faeffe9
Jacqueline Abdo <jfabdo@gmail.com>
2020-12-05 14:01:47 -0700

    Serverfile created

---
+++ server.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", services.HandleSe)
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
