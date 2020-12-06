9c34be63e297572f97629201bb53b752cfecf8dc
Jacqueline Abdo <jfabdo@gmail.com>
2020-12-06 10:06:49 -0700

    wrting queue handling

--- src/api/api.go
+++ src/api/api.go
@@ -2,12 +2,24 @@ package api
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/go-redis/redis"
 )
 
+type indmessage struct {
+	name    string
+	message string
+	time    time.Time
+}
+
 var ctx = context.Background()
+
+var q = map[string][]indmessage{
+	"message": make([]indmessage, 1000),
+	"fork":    make([]indmessage, 1000)}
 
 func getClient() *redis.Client {
 	return redis.NewClient(&redis.Options{
@@ -17,38 +29,44 @@ func getClient() *redis.Client {
 	})
 }
 
+var rdb = getClient()
+
+//doing it like this so that when Redis is implemented in a bit I don't have to change a lot of code
+func pushToQueue(topic string, message indmessage) {
+	// err := rdb.Set(topic, message, 0).Err() //set key to ????, and set value to message
+	// if err != nil {
+	// 	panic(err)
+	// }
+	q[topic] = append(q[topic], message)
+}
+
 //Index handles messages going to the index
 func Index(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		http.Error(w, "Method is not supported.", http.StatusNotFound)
 		return
 	}
+	//return the web page here
+
 }
 
 func fork(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
+
+	var msg indmessage
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
 		return
 	}
-
-	rdb := getClient()
-
-	err := rdb.Set("key", "value", 0).Err() //set key to ????, and set value to message
-	if err != nil {
-		panic(err)
-	}
+	pushToQueue("fork", msg)
 }
 
 func message(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
+	var msg indmessage
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
 		return
 	}
-
-	rdb := getClient()
-
-	err := rdb.Set("key", "value", 0).Err() //set key to ????, and set value to message
-	if err != nil {
-		panic(err)
-	}
+	pushToQueue("message", msg)
 }
