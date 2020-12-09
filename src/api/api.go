993afc36dcd35fbf8b7ad8518a46eb6909a45db3
Jacqueline Abdo <jfabdo@gmail.com>
2020-12-09 09:33:11 -0700

    Queuing and fetching the queue are complete

--- src/api/api.go
+++ src/api/api.go
@@ -1,10 +1,9 @@
 package api
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
-	"strings"
+	"time"
 
 	"github.com/mediocregopher/radix/v3"
 )
@@ -12,54 +11,42 @@ import (
 var channel = "0000000000001"
 
 type indmessage struct {
-	UN  string
-	Msg string
-	To  string
-	// time    time.Time
+	UN   string
+	Msg  string
+	To   string
+	Time time.Time
 }
 
 type tryme struct {
 	Stuff indmessage
 }
 
-var ctx = context.Background()
-
-var q = map[string][]indmessage{
-	"message": make([]indmessage, 1000),
-	"fork":    make([]indmessage, 1000)}
-
-func getClient() *radix.Pool {
-	pool, err := radix.NewPool("tcp", "localhost:6379", 10)
-	if err != nil {
-		// handle error
-	}
-	return pool
-}
-
 //doing it like this so that when Redis is implemented in a bit I don't have to change a lot of code
 func pushToQueue(topic string, message map[string]string) {
 	var structmsg indmessage
 	var rdb = getClient()
+	structmsg.Time = time.Now()
 	structmsg.Msg = message["message"]
 	structmsg.UN = message["name"]
 	structmsg.To = message["for"]
 
 	tryme, err := json.Marshal(structmsg)
 	if err != nil {
-		//stuff
+		println(err)
+		panic(err)
 	}
-	rdb.Do(radix.Cmd(nil, "publish", topic+channel+structmsg.UN, string(tryme))) //, //string(tryme)))
-	rdb.Do(radix.Cmd(nil, "lpush", topic+channel+structmsg.UN, string(tryme)))
+	hashme := asSha256(string(tryme))
+	sendme := make(map[string]string)
+	sendme[hashme] = string(tryme)
+	final, err := json.Marshal(sendme)
+	rdb.Do(radix.Cmd(nil, "publish", "chat"+topic+channel+structmsg.UN, string(final))) //, //string(tryme)))
+	rdb.Do(radix.Cmd(nil, "lpush", "chat"+topic+channel+structmsg.UN, string(final)))
 }
 
 //Index handles messages going to the index
 func Index(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
-		return
-	}
+	Messaging(w, r)
 	//return the web page here
-
 }
 
 //Messaging handles all messaging requests, pushing them to either fork, message, or
@@ -74,6 +61,13 @@ func Messaging(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 400)
 		return
 	}
+	switch r.URL.Path {
+	case "/fork":
+		pushToQueue("fork", msg)
+	case "/message":
+		pushToQueue("message", msg)
+	case "/history":
+		CheckMessages(w, msg)
+	}
 
-	pushToQueue(strings.Split(r.URL.Path, "/")[2], msg)
 }
--- src/api/extras.go
+++ src/api/extras.go
@@ -1,8 +1,58 @@
 package api
 
-import "net/http"
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
-func CheckMessages(w http.ResponseWriter, r *http.Request) {
-	
+	"github.com/mediocregopher/radix/v3"
+)
 
+var ctx = context.Background()
+
+func asSha256(o interface{}) string {
+	h := sha256.New()
+	h.Write([]byte(fmt.Sprintf("%v", o)))
+
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func getClient() *radix.Pool {
+	pool, err := radix.NewPool("tcp", "localhost:6379", 10)
+	if err != nil {
+		println(err)
+		panic(err)
+	} else {
+		return pool
+	}
+}
+
+// CheckMessages checks the incoming list of messages and makes sure that
+func CheckMessages(w http.ResponseWriter, messages map[string]string) {
+	var rdb = getClient()
+
+	// var messagedict map[string]string
+	var messagelist []string
+	err := rdb.Do(radix.Cmd(&messagelist, "lrange", "chat"+"message"+channel+messages["name"], "0", "10"))
+	if err != nil {
+		println(err)
+		panic(err)
+	}
+	for i := range messages {
+		for j := range messagelist {
+			if messages[i] == messagelist[j] {
+				copy(messagelist[j:], messagelist[j+1:])
+				messagelist = messagelist[:len(messagelist)-1]
+			}
+		}
+	}
+
+	rspns, err := json.Marshal(&messagelist)
+	if err != nil {
+		//do somethign
+	}
+
+	fmt.Fprintf(w, string(rspns))
 }
